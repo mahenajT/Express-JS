77949eae4e90e8ae030b7f68520601ba72443d38
mahenaj <mahenaj200815@gmail.com>
2024-07-15 07:54:47 +0600

    2 Application Json

---
+++ 5_Post_Request/2_Application_Json.js
@@ -0,0 +1,35 @@
+// Install body-parser node Package
+const express = require("express");
+const bodyParser = require("body-parser");
+let app = express();
+app.use(bodyParser.json()); // for parsing application/json()
+
+
+
+
+
+// Send JSON raw Data in JSON body
+app.post("/", (req, res) => {
+  let jsonData = req.body;
+  let JSONString = JSON.stringify(jsonData);
+  res.send(JSONString);
+});
+
+
+
+
+
+// Specific Key catch then Show the value in The Response
+app.post("/one", (req, res) => {
+  let jsonData = req.body;
+  let name = jsonData.name;
+  res.send(name);
+});
+
+
+
+
+
+app.listen(8010, function () {
+  console.log("Successfully running");
+});
